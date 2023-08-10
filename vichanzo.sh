14555406820cbd94153cef8ff05cfb1a85b5119e
ViChanzo <75827403+vichanzo@users.noreply.github.com>
2023-08-09 23:09:51 -0700

    Update vichanzo.sh

--- vichanzo.sh
+++ vichanzo.sh
@@ -8,7 +8,7 @@ git clone git@github.com:vichanzo/PFsense_Proxmox.git
 wget https://atxfiles.netgate.com/mirror/downloads/pfSense-CE-2.7.0-RELEASE-amd64.iso.gz?_gl=1*3rkkdf*_ga*NDk4MDQ4NzE4LjE2OTE2NDQ0ODE.*_ga_TM99KBGXCB*MTY5MTY0NDQ4MS4xLjAuMTY5MTY0NDQ4MS42MC4wLjA.
 
 # Extract the ISO
-gunzip -c pfSense-CE-2.7.0-RELEASE-amd64.iso.gz > /var/lib/images/ISO/pfsense.iso
+gunzip -c pfSense-CE-2.7.0-RELEASE-amd64.iso.gz > /var/lib/vz/template/iso/pfsense.iso
 
 cd PFsense_Proxmox
 # ansible-playbook local.yml
